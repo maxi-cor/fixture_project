ed408c57f8582eb5408c5e1d52000c721fd0de90
maxi <ecornalba@estudiolevex.com.ar>
2024-02-08 17:14:15 -0300

    ading the call to the API and aplie a console.log

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const apiKey = `https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=a4f5b4223f9e3010cc74e81538e9200e2410e3319a1c21bba5dfb7acc7a8e4bb&from=2024-02-08&to=2024-02-08`;
+
+  const [info, setInfo] = useState(null)
+
+  const bringInfo = async () => {
+    try {
+      const response = await fetch(apiKey);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        setInfo(data.result);
+
+    } catch (error) {
+      console.log('Error: ', error)
+    }
+  }
+
+  useEffect(() => {
+    bringInfo()
+  },[])
+
+  console.log(info)
+
   return (
     <div className="App">
         <h1>Hello word here i GO!!</h1>
